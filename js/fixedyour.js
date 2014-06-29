d874814ede7c00523eb7984f13aa679112cd044e
Jan Thomas Hanto <jthanto@gmail.com>
2014-06-29 22:33:55 +0200

    Design Alpha

--- js/fixedyour.js
+++ js/fixedyour.js
@@ -3,5 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+$(document).ready(function() {
+    //Initializing bigSlide menu;
+    $('.menu-link').bigSlide();
+});
