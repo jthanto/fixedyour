249a5c2be74da309aa092cf54906a26a38b2c240
jthanto <jthanto@fixedyour.net>
2016-05-01 21:00:38 +0200

    Changed some visuals concerning the contact form, and added a svg instead of the messy background: magix happens stuff, that scaled differently in ff and chrome. Also my mailclass now uses a default email address.

--- _config/config.tpl.php
+++ _config/config.tpl.php
@@ -6,5 +6,6 @@ define('SMTP_PORT', 587); // your smtp port
 define('SMTP_DEBUG_LEVEL', 0); // your wanted debug level
 define('SMTP_USERNAME', ''); // your username
 define('SMTP_PASSWORD', ''); // your password
+define('DEFAULT_FROM_MAIL', 'jthanto@fixedyour.net');
 
 ?>
