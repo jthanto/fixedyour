a706d41e0a9f40fc3ea64808dbc52f6496778a13
Jan Thomas Hanto <janth@cloudmaster.no>
2021-04-19 01:06:58 +0200

    The smallest change.

--- Blog/View/blog.php
+++ Blog/View/blog.php
@@ -1,15 +1,15 @@
 <h1>Heiaaa bloggen</h1>
 
+<p>Dere har sikkert skjønt det. Dette skal forestille en blogg. Hele den "Heiaaa bloggen"-greia. Her kommer jeg til å ytre meg om både det ene, og det andre.
+    Blir nok mest vås vil jeg tro. Vil du høre meg snakke om noe fornuftig - i hvert fall innimellom, kan du sjekke ut podcasten jeg har sammen med en kompis.
+    <a href="http://sutrepodden.no" target="_blank">Sutrepodden</a> heter den, og du finner den overalt du hører podcast. Hvis ikke kan du jo gi meg en lyd, så har jeg mer å styre med.</p>
 <? //todo: add categories ?>
-
-<div>
-    <p></p>
-</div>
-
+<hr>
+<h3>Siste blogginnlegg</h3>
 <div class="row">
-
 <? foreach($posts as $idx => $post): ?>
     <div class="col-12 col-md-6 col-lg-4">
+        <div class="p-2"></div>
         <div class="card">
             <img class="card-img-top" src="/img/blog/<?=$post['url']?>.jpg" alt="random image">
             <div class="card-body">
